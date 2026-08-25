ecb61310ee17f337825b00e5948bbbb50ff3019e
cyrowolf45 <jospkiki@gmail.com>
2026-08-25 17:19:47 +0200

    Item versuch

--- src/main/java/noxfer/biomsword/net/BIomSword.java
+++ src/main/java/noxfer/biomsword/net/BIomSword.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.minecraft.resources.ResourceLocation;
 
+import net.minecraft.world.item.Item;
+import noxfer.biomsword.net.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,16 +20,11 @@ public class BIomSword implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        ModItems.registerModItems();
+
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-        ServerEntityEvents.ENTITY_LOAD.register((entity, serverLevel) -> {
-
-        });
-
-
-
 
 
 
---
+++ src/main/java/noxfer/biomsword/net/BIomSwordClient.java
@@ -0,0 +1,10 @@
+package noxfer.biomsword.net;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BIomSwordClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
