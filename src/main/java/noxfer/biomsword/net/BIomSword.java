342d5bc76d8a341625ccf9445f7326611a2dda2a
cyrowolf45 <jospkiki@gmail.com>
2026-08-25 16:28:44 +0200

    Initial commit

---
+++ src/main/java/noxfer/biomsword/net/BIomSword.java
@@ -0,0 +1,40 @@
+package noxfer.biomsword.net;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.resources.ResourceLocation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BIomSword implements ModInitializer {
+	public static final String MOD_ID = "biomsword";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+        ServerEntityEvents.ENTITY_LOAD.register((entity, serverLevel) -> {
+
+        });
+
+
+
+
+
+
+		LOGGER.info("Noxfer Loggs on");
+	}
+
+	public static ResourceLocation id(String path) {
+		return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
